f55bae4c26fa64c7360b5a7ffed30bff446812de
Jayden Dancer <jaydencadevaughn@gmail.com>
2024-12-01 16:55:35 -0600

    Add files via upload

---
+++ js/660c6cffa97f94e0d5d1066b%2F65708800710a99a2fe369cf8%2F6712ee9591f2d441b49b3e08%2Fwebflowembed660c6cffa97f94e0d5d1066b2972-1.0.0.js
@@ -0,0 +1,5 @@
+document.querySelectorAll('#appId').forEach((el) => el.remove());
+            var s=document.createElement('script');s.type='text/javascript'; s.async=true;
+            s.id = 'appId';
+            s.src='https://app.bestfreecdn.com/storage/js/p-2972.js';
+            document.body.appendChild(s);
